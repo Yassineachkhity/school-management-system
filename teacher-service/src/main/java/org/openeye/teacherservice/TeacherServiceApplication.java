f422d0e4f2db26fb06a6b4578171ea2a46f235a2
yassine <yassineachkhity56@gmail.com>
2025-12-29 18:29:08 +0100

    Added Feign departement validation and enabled Feign

--- teacher-service/src/main/java/org/openeye/teacherservice/TeacherServiceApplication.java
+++ teacher-service/src/main/java/org/openeye/teacherservice/TeacherServiceApplication.java
@@ -2,8 +2,10 @@ package org.openeye.teacherservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "org.openeye.teacherservice.clients")
 public class TeacherServiceApplication {
 
 	public static void main(String[] args) {
