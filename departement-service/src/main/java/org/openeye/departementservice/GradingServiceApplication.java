d6ab0613fdffc2a61b4046a4d3ab03003d840728
yassine <yassineachkhity56@gmail.com>
2025-12-29 16:07:56 +0100

    deleting GradingService setting DepartementService Implementing CourseService

--- departement-service/src/main/java/org/openeye/departementservice/GradingServiceApplication.java
+++ departement-service/src/main/java/org/openeye/departementservice/GradingServiceApplication.java
@@ -1,4 +1,4 @@
-package org.openeye.gradingservice;
+package org.openeye.departementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- departement-service/src/test/java/org/openeye/departementservice/GradingServiceApplicationTests.java
+++ departement-service/src/test/java/org/openeye/departementservice/GradingServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package org.openeye.gradingservice;
+package org.openeye.departementservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
